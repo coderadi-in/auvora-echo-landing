34431dfd1060fef5184ed527c43675623a05a289
coderadi-in <adiprofitcoder@gmail.com>
2026-09-03 20:26:26 +0530

    Fixed form submission logic.

--- scripts/form.js
+++ scripts/form.js
@@ -2,14 +2,10 @@
 // ELEMENT REFERENCE
 // ==================================================
 
-const successPage = document.getElementById('successPage');
-const hideSuccessPage = document.querySelector('.success-page .btn');
-
+// ----- REGISTRATION SPECIFIC ----- //
 const registrationForm = document.getElementById('registrationForm');
-const enquiryForm = document.getElementById('enquiryForm');
-
-const registrationFormTrigger = document.getElementById('registrationFormTrigger');
 const closeRegistrationForm = document.getElementById('closeRegistrationForm');
+const registrationFormTrigger = document.getElementById('registrationFormTrigger');
 const registrationSubmit = document.getElementById('registrationSubmit');
 
 const registrationName = document.getElementById('registrationName');
@@ -17,8 +13,10 @@ const registrationEmail = document.getElementById('registrationEmail');
 const registrationPhone = document.getElementById('registrationPhone');
 const registrationBusiness = document.getElementById('registrationBusiness');
 
-const enquiryFormTrigger = document.getElementById('enquiryFormTrigger');
+// ----- ENQUIRY SPECIFIC ----- //
+const enquiryForm = document.getElementById('enquiryForm');
 const closeEnquiryForm = document.getElementById('closeEnquiryForm');
+const enquiryFormTrigger = document.getElementById('enquiryFormTrigger');
 const enquirySubmit = document.getElementById('enquirySubmit');
 
 const enquiryName = document.getElementById('enquiryName');
@@ -26,145 +24,146 @@ const enquiryEmail = document.getElementById('enquiryEmail');
 const enquiryPhone = document.getElementById('enquiryPhone');
 const enquiryMessage = document.getElementById('enquiryMessage');
 
+// ----- POPOVER SPECIFIC ----- //
+const successPage = document.getElementById('successPage');
+const failurePage = document.getElementById('failurePage');
+const closePopovers = document.querySelectorAll('.popover .btn');
+
 // ==================================================
 // FUNCTIONS
 // ==================================================
 
-// * FUNCTION TO TOGGLE REGISTRATION SEND-BTN STATE
-function toggleRegistration(btn) {
-    btn.classList.toggle('disabled', registrationName.value.trim() === '' || registrationEmail.value.trim() === '' || registrationPhone.value.trim() === '' || registrationBusiness.value.trim() === '');
-}
-
-// * FUNCTION TO TOGGLE ENQUIRY SEND-BTN STATE
-function toggleEnquiry(btn) {
-    btn.classList.toggle('disabled', enquiryName.value.trim() === '' || enquiryEmail.value.trim() === '' || enquiryPhone.value.trim() === '' || enquiryMessage.value.trim() === '');
-}
-
-// * FUNCTION TO UPDATE INPUT STATES
-function updateInputState(elem) {
-    if (elem.value.trim() === '') {
-        elem.style.borderColor = 'var(--color-state-red)';
-        const msg = elem.parentElement.querySelector('.err-msg');
-        if (!msg) { return; }
-        msg.classList.remove('hidden');
-        msg.textContent = 'This field is required';
-    }
-
-    else {
-        elem.style.borderColor = 'var(--color-state-green)';
-        const msg = elem.parentElement.querySelector('.err-msg');
-        if (!msg) { return; }
-        msg.classList.add('hidden');
-        msg.textContent = '';
-    }
-}
-
-// * FUNCTION TO CREATE A FORM DATA OBJECT
-function createFormData(entries) {
-    const formData = new FormData();
-
-    for (const [key, value] of Object.entries(entries)) {
-        formData.append(key, value.value);
-    }
-
-    return formData;
-}
-
-// * FUNCTION TO RESET INPUTS
-function resetInputs(inputs) {
-    inputs.forEach(element => {
-        element.value = '';
+// * FUNCTION TO UPDATE BUTTON STATUS
+function updateBtnStatus(inputs, btn) {
+    let validated = true;
+    inputs.forEach(input => {
+        if (input.value.trim() === '') {
+            input.style.borderColor = 'var(--color-state-red)';
+            validated = false;
+        }
     });
+    if (validated) {
+        btn.classList.remove('disabled');
+        return true;
+    } else { return false; }
 }
 
-// * FUNCTION TO ADD EVENT LISTENER FOR UPDATING INPUT STATE
-function addUpdateStateChanger(input, submitBtn) {
-    input.addEventListener('input', () => {
-        updateInputState(input);
-        toggleSendBtnState(submitBtn);
-    });
-}
-
-// * FUNCTION TO SUBMIT A FORM
-function addSubmissionListener(submitBtn, entries, submitURL) {
-    const inputs = Object.values(entries);
-
-    submitBtn.addEventListener('click', () => {
-        let isValid = true;
-
-        // Validate inputs
-        inputs.forEach((input) => {
-            if (input.value.trim() == '') {
-                isValid = false;
-                updateInputState(input);
+// * FUNCTION TO CREATE INPUT VALIDATOR
+function createInputValidator(inputs, btn) {
+    inputs.forEach(input => {
+        input.addEventListener('input', () => {
+            if (input.value.trim() === '') {
+                input.style.borderColor = 'var(--color-state-red)';
+                btn.classList.add('disabled');
+                return;
+            } else {
+                input.style.borderColor = 'var(--color-state-green)';
+                updateBtnStatus(inputs, btn);
             }
         });
-
-        if (!isValid) {
-            submitBtn.classList.add('shake');
-            setTimeout(() => {
-                submitBtn.classList.remove('shake');
-            }, 2000);
-            return;
-        }
-
-        // Submit form
-        try {
-            const formData = createFormData(entries);
-            fetch(submitURL, {
-                method: 'POST',
-                mode: 'no-cors',
-                body: formData
-            });
-            resetInputs(inputs);
-
-            submitBtn.classList.remove('open');
-            setTimeout(() => {
-                successPage.classList.add('active');
-            }, 400);
-
-        } catch {
-            submitBtn.style.borderColor = 'var(--color-state-red)';
-            submitBtn.textContent = "Error submitting form.";
-        }
     });
+}
+
+// * FUNCTION TO OPEN/CLOSE FORM
+function addToggleFormListener(form, trigger, status = 'open') {
+    trigger.addEventListener('click', () => {
+        if (status === 'open') {
+            form.classList.add('active');
+        } else {
+            form.classList.remove('active');
+        }
+    })
+}
+
+// * FUNCTION TO CREATE CLOSE POPOVER LISTENER
+function addClosePopoverListener(triggers) {
+    triggers.forEach(trigger => {
+        trigger.addEventListener('click', () => {
+            document.querySelectorAll('.popover').forEach(popover => {
+                popover.classList.remove('active');
+            });
+        });
+    });
+}
+
+// * FUNCTION TO CREATE A FORM
+function createForm(entries) {
+    const keys = Object.keys(entries);
+    const form = new FormData();
+
+    keys.forEach(key => {
+        form.append(key, entries[key].value);
+    });
+
+    return form;
+}
+
+// * FUNCTION TO HANDLE SUBMISSION BUTTON CLICK
+async function handleSubmission(btn, entries, submissionURL) {
+    if (btn.classList.contains('disabled')) {
+        btn.classList.add('shake');
+        setTimeout(() => {
+            btn.classList.remove('shake');
+        }, 700);
+        return;
+    }
+
+    let validated = updateBtnStatus(Object.values(entries), btn);
+    if (!validated) return;
+
+    const body = createForm(entries);
+
+    try {
+        await fetch(submissionURL, {
+            body: body,
+            method: 'POST',
+            mode: 'no-cors',
+        });
+
+        successPage.classList.add('active');
+    } catch (error) {
+        failurePage.classList.add('active');
+    }
 }
 
 // ==================================================
 // EVENT LISTENERS
 // ==================================================
 
-// & EVENT LISTENER FOR FORM STATE TOGGLE
-formTrigger.addEventListener('click', () => {
-    form.classList.add('active');
+// & INITIAL DISPLAY SETTINGS
+document.addEventListener('DOMContentLoaded', () => {
+    createInputValidator([
+        registrationName, registrationEmail,
+        registrationPhone, registrationBusiness,
+    ], registrationSubmit);
+
+    createInputValidator([
+        enquiryName, enquiryEmail,
+        enquiryPhone, enquiryMessage,
+    ], enquirySubmit);
+})
+
+// & EVENT LISTENERS FOR FORM TOGGLE
+addToggleFormListener(registrationForm, registrationFormTrigger, 'open');
+addToggleFormListener(registrationForm, closeRegistrationForm, 'close');
+addToggleFormListener(enquiryForm, enquiryFormTrigger, 'open');
+addToggleFormListener(enquiryForm, closeEnquiryForm, 'close');
+
+// & EVENT LISTENERS FOR CLOSE POPOVER
+addClosePopoverListener(closePopovers);
+
+// & EVENT LISTENER FOR REGISTRATION SUBMISSION
+registrationSubmit.addEventListener('click', () =>{
+    handleSubmission(registrationSubmit, {
+        'entry.1918707273': registrationName, 'entry.1654067689': registrationEmail,
+        'entry.700167914': registrationPhone, 'entry.1259074432': registrationBusiness,
+    }, 'https://docs.google.com/forms/d/e/1FAIpQLSe5k_scVs7FVaPFfD6KgNR3T7wGEa6MZPDKXUvp-KA1bEZ6XA/formResponse');
 });
 
-// & EVENT LISTENER FOR FORM STATE TOGGLE
-closeForm.addEventListener('click', () => {
-    form.classList.remove('active');
+// & EVENT LISTENER FOR ENQUIRY SUBMISSION
+enquirySubmit.addEventListener('click', () =>{
+    handleSubmission(enquirySubmit, {
+        'entry.1551876098': enquiryName, 'entry.1204135284': enquiryEmail,
+        'entry.1855383320': enquiryPhone, 'entry.1083720747': enquiryMessage,
+    }, 'https://docs.google.com/forms/d/e/1FAIpQLSfTXjt5uYNt296mS8vLoXKfgS47EMcyYJdvsb432ZEy3IvKwA/formResponse');
 });
-
-// & EVENT LISTENER FOR INPUT VALIDATION
-addUpdateStateChanger(username, submitBtn);
-addUpdateStateChanger(email, submitBtn);
-addUpdateStateChanger(phone, submitBtn);
-addUpdateStateChanger(business, submitBtn);
-
-// & EVENT LISTENER FOR SUCCESS PAGE CLOSE
-hideSuccessPage.addEventListener('click', () => {
-    successPage.classList.remove('active');
-});
-
-// & EVENT LISTENER FOR FORM SUBMISSION
-addSubmissionListener(
-    submitBtn,
-    {
-        'entry.1918707273': username,
-        'entry.1654067689': email,
-        'entry.700167914': phone,
-        'entry.1259074432': business,
-    },
-    'https://docs.google.com/forms/d/e/1FAIpQLSe5k_scVs7FVaPFfD6KgNR3T7wGEa6MZPDKXUvp-KA1bEZ6XA/formResponse'
-);
-
-// https://docs.google.com/forms/d/e/1FAIpQLSfTXjt5uYNt296mS8vLoXKfgS47EMcyYJdvsb432ZEy3IvKwA/viewform?usp=pp_url&entry.1551876098=ADI&entry.1204135284=adi@coderadi.in&entry.1855383320=000&entry.1083720747=I+don't+know
